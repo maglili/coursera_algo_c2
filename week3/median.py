1db9ffb63d6ce9ee0b2c615436ec9cb36ca949da
maglili <gibson870811@gmail.com>
2021-11-05 00:12:25 +0800

    working on median

--- week3/median.py
+++ week3/median.py
@@ -1,8 +1,42 @@
+import heapq
+
+# Input files
 path = "./Median.txt"
 nums = []
 with open(path, "r") as f:
     for row in f:
         nums.append(int(row))
 
-min_h = []
-max_h = []
+# min / max heap
+h_low = []  # half - lower values, max heap
+h_high = []  # half - larger values, min heap
+add_counter = 0
+
+for num in nums:
+    # initial condition
+    if h_low == []:
+        h_low.append(-1 * num)
+        heapq.heapify(h_low)
+        continue
+
+    # num > max of h_low
+    if num > -1 * h_low[0]:
+        if h_high == []:
+            h_high.append(num)
+            heapq.heapify(h_high)
+        else:
+            heapq.heappush(h_high, num)
+
+    # num <= max of h_low
+    else:
+        if add_counter < 1:
+            heapq.heappush(h_low, -1 * num)
+            add_counter += 1
+        else:
+            l_max = -1 * heapq.heappop(h_low)
+            heapq.heappush(h_high, l_max)
+            heapq.heappush(h_low, -1 * num)
+            add_counter = 0
+
+print(len(h_low))
+print(len(h_high))
